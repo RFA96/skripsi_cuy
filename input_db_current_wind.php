2c3d2681e210f58225e073f1fd0f25487e410265
Raka Flyhigh Abdurrahman <raka.flyhigh@github.com>
2018-05-22 07:53:24 +0700

    Tambahin angin (2)

---
+++ input_db_current_wind.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raka_matsukaze
+ * Date: 5/22/18
+ * Time: 07:50 AM
+ */
+    include "db_connection.php"; include "decode_xml_current_weather.php";
+
+    date_default_timezone_set("Asia/Jakarta");
+    $windSpeedFinal = floatval($getWindSpeed) * 3.6;
+    $windSpeedFinal_2 = number_format($windSpeedFinal, 2);
+
+    mysqli_query("INSERT INTO wind(wind_speed, wind_direction) VALUES '$windSpeedFinal_2', '$getWindDirection'");
+?>
