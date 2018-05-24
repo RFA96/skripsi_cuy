46470e44fba260de2ee9f781aec2c268a782920a
Raka Flyhigh Abdurrahman <raka.flyhigh@github.com>
2018-05-24 10:59:50 +0700

    Tambahin angin (4)

--- input_db_current_weather.php
+++ input_db_current_weather.php
@@ -8,8 +8,10 @@
     include "db_connection.php"; include "decode_xml_current_weather.php";
 
     date_default_timezone_set("Asia/Jakarta");
-//    echo "Temperature: ".$getTemperature."<br>";
-//    echo "Humidity: ".$getHumidity."<br>";
+
+    $windSpeedFinal = floatval($getWindSpeed) * 3.6;
+    $windSpeedFinal_2 = number_format($windSpeedFinal, 2);
 
     mysqli_query($conn, "INSERT INTO suhu_kelembapan(temperature, humidity, tanggal, waktu) VALUES ('".(int) $getTemperature."', '$getHumidity', '".date("Y-m-d")."', '".date("H:i:s")."')");
+    mysqli_query($conn,"INSERT INTO wind(wind_speed, wind_direction) VALUES ('$windSpeedFinal_2', '$getWindDirection')");
 ?>
